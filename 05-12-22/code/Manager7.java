81ea6d1aacf170e0b5306695eae865da1c93f44c
Sadhak26 <119845910+Sadhak26@users.noreply.github.com>
2022-12-05 14:06:46 +0530

    Add files via upload

---
+++ 05-12-22/code/Manager7.java
@@ -0,0 +1,19 @@
+//age evaculation
+class Manager7{
+           public static void main(String[]args)
+           {
+             
+             int agelimit=18;
+			 int age = 170;
+			  if (age>agelimit)
+			  {
+			  
+			  System.out.println("you are eligible for voting");
+			  }
+			 else
+			 {
+			 System.out.println("you are not eligible for voting");
+            } 
+			}
+
+}
