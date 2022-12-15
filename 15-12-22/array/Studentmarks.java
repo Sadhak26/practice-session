2c23e31318d631e4f79f2b0cc1b6801940366fbf
Sadhak26 <119845910+Sadhak26@users.noreply.github.com>
2022-12-15 14:05:17 +0530

    Add files via upload

---
+++ 15-12-22/array/Studentmarks.java
@@ -0,0 +1,17 @@
+class Studentmarks
+{
+public static void main(String[]args)
+{
+int a[][]={{59,55,60},{62,60,65}};
+for(int rows=0;rows<a.length;rows++);
+{
+for(int cols=0;cols<a.length;cols++);
+{
+
+
+}
+System.out.println("Marks:"+a[rows]);
+System.out.println("Marks:"+a[cols]);
+}
+}
+}
